1ac23f12ce4f2078c99ce0570e33c7bfb447cf88
Jeff Levine <jlevine_84@yahoo.com>
2019-01-26 12:19:06 -0500

    clean up

--- assets/javascript/timesheet.js
+++ assets/javascript/timesheet.js
@@ -1,16 +1,14 @@
-<script src="https://www.gstatic.com/firebasejs/5.8.1/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCVonF7CLfBq7JzkbwBDndqrrOoykjR-0E",
-    authDomain: "timesheet-742ad.firebaseapp.com",
-    databaseURL: "https://timesheet-742ad.firebaseio.com",
-    projectId: "timesheet-742ad",
-    storageBucket: "",
-    messagingSenderId: "30282190889"
-  };
-  firebase.initializeApp(config);
-</script>
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCVonF7CLfBq7JzkbwBDndqrrOoykjR-0E",
+authDomain: "timesheet-742ad.firebaseapp.com",
+databaseURL: "https://timesheet-742ad.firebaseio.com",
+projectId: "timesheet-742ad",
+storageBucket: "",
+messagingSenderId: "30282190889"
+};
+firebase.initializeApp(config);
+
 
 // On click of submit button, grab input from the forms, assign to variable names, set the variables in the database,
 // append variable names to html table
@@ -32,27 +30,27 @@ function resetVals() {
 
 $(document).ready(function(){
 
-$("form").submit(function(event){
-    console.log('got here');
-// prevents the page from executing a full refresh on the click of the submission
-event.preventDefault();
+    $("form").submit(function(event){
+        console.log('got here');
+    // prevents the page from executing a full refresh on the click of the submission
+    event.preventDefault();
 
-// Assigin the user input values to the name, role rate, start_date variables
-name = $("#input-name").val(); 
-role = $("#input-role").val(); 
-rate = $("#input-monthly-rate").val(); 
-start_date = $("#input-start-date").val(); 
+    // Assigin the user input values to the name, role rate, start_date variables
+    name = $("#input-name").val(); 
+    role = $("#input-role").val(); 
+    rate = $("#input-monthly-rate").val(); 
+    start_date = $("#input-start-date").val(); 
 
-    // establish variables in database
-    database.ref().add({
-        name: name,
-        role: role,
-        rate: rate,
-        start_date: start_date
-    });
+        // establish variables in database
+        database.ref().add({
+            name: name,
+            role: role,
+            rate: rate,
+            start_date: start_date
+        });
 
-// Close on submit function
-})
+    // Close on submit function
+    })
 
 // create event listener for when a child is added to the database via a user form submission
 database.ref().on("child_added", function (snapshot) {
